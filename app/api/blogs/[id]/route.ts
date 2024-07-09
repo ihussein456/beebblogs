eecad8d60fe252d2b05a39dc03e42cdfc923af8c
Ibrahim <ihussein456@gmail.com>
2024-07-09 13:14:51 +0100

    build fix

--- app/api/blogs/[id]/route.ts
+++ app/api/blogs/[id]/route.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import connectMongoDB from '@/lib/db';
 import Posts from '@/models/blog';
 import { NextApiRequest } from 'next';
 
 
-export async function GET(req: NextApiRequest, {params}: {params: {id: string}}){
+export async function GET(req: NextRequest, {params}: {params: {id: string}}){
     const { id } = params;
     await connectMongoDB();
     const post = await Posts.findOne({ _id: id });
